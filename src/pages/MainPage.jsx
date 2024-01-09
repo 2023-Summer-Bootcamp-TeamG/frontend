6fde9213406121e6eb8d31c7b0322f80b1dc011b
Joo YeongJun <154403139+Ye0ngjun@users.noreply.github.com>
2024-01-10 03:11:08 +0900

    feat:메인페이지 테스트용 제거

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,13 +1,3 @@
-import Next from '../components/Common/Next';
-import TeamName from '../components/Common/TeamName';
-import TopNav from '../components/Common/TopNav';
-
 export default function MainPage() {
-  return (
-    <div>
-      <TeamName />
-      <TopNav />
-      <Next />
-    </div>
-  );
+  return <div>hi</div>;
 }
