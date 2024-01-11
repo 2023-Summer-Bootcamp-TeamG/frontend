f0e8faf4e22b6206ca8cb4c3116052431b09fac8
hwang <hgh1057@gmail.com>
2024-01-12 00:23:02 +0900

    fix: 코드 수정

--- src/components/Common/TeamName.jsx
+++ src/components/Common/TeamName.jsx
@@ -1,7 +1,7 @@
 export default function TeamName() {
   return (
     <div
-      className="text-center font-black p-5 text-2xl italic"
+      className="text-center font-black p-5 text-2xl italic h-20"
       style={{
         textShadow: '0px 4px 4px rgba(0, 0, 0, 0.5)',
         WebkitTextStroke: '1.2px white',
