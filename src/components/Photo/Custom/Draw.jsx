f437c7f7ee3a8eb2e19e7d2c913da4b6954d76dd
Joo YeongJun <154403139+Ye0ngjun@users.noreply.github.com>
2024-01-12 17:55:11 +0900

    feat 커스텀페이지 퍼블리싱

---
+++ src/components/Photo/Custom/Draw.jsx
@@ -0,0 +1,9 @@
+import custum from '../../../assets/custum.png';
+
+export default function Draw() {
+  return (
+    <div>
+      <img src={custum} alt="custom" className="text-center ml-24 h-[28rem]" />
+    </div>
+  );
+}
