e9fe0ae9c35d146f29ea601ee3a6b5ffde1b48e9
hwang <hgh1057@gmail.com>
2024-01-17 23:20:25 +0900

    feat: 커스텀페이지(sticker)

--- src/components/Photo/Custom/Stickers.jsx
+++ src/components/Photo/Custom/Stickers.jsx
@@ -1,7 +1,57 @@
+import { useState } from 'react';
+
+import DetailStickers from './Sticker/DetailSticker';
+import Icon from './Sticker/Icon';
+
 export default function Stickers() {
+  const [isStickersModalOpen, setStickersModalOpen] = useState(false);
+  const [title, setTitle] = useState('');
+
+  const closeStickersModal = () => {
+    setStickersModalOpen(false);
+  };
+
   return (
-    <div className="bg-white mt-4 p-4 h-[33rem] w-[19rem] rounded-xl border-black border shadow-[0_4px_12.3px_0px_rgba(0,0,0,0.3)]">
-      스티커
+    <div>
+      {isStickersModalOpen ? (
+        <DetailStickers onClose={closeStickersModal} title={title} />
+      ) : (
+        <div className="bg-white mt-4 p-6 h-[30rem] w-[19rem] rounded-xl border-black border shadow-[0_4px_12.3px_0px_rgba(0,0,0,0.3)]">
+          <div className="font-bold ml-2 m-1 text-lg">스티커</div>
+          <div className=" grid grid-cols-3 ">
+            <Icon
+              icon="😀"
+              title="기본티콘"
+              setOpen={setStickersModalOpen}
+              setTitle={setTitle}
+            />
+            <Icon
+              icon="👾"
+              title="픽셀티콘"
+              setOpen={setStickersModalOpen}
+              setTitle={setTitle}
+            />
+            <Icon
+              icon="✨"
+              title="기호"
+              setOpen={setStickersModalOpen}
+              setTitle={setTitle}
+            />
+            <Icon
+              icon="🧸"
+              title="캐릭터"
+              setOpen={setStickersModalOpen}
+              setTitle={setTitle}
+            />
+            <Icon
+              icon="🗂️"
+              title="나만의 스티커"
+              setOpen={setStickersModalOpen}
+              setTitle={setTitle}
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
