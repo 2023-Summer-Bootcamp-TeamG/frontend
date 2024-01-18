e18cc83522ed95cff151986b67d6c6b73dac9cc2
Joo YeongJun <154403139+Ye0ngjun@users.noreply.github.com>
2024-01-18 15:27:24 +0900

    feat기본 프레임 6개 퍼블리싱

---
+++ src/components/Photo/BasicFrame/Frame2l.jsx
@@ -0,0 +1,12 @@
+export default function Frame2l() {
+  return (
+    <div>
+      <div className="bg-black w-[23rem] h-[32rem] items-center flex flex-col">
+        <div className="m-3 bg-white w-[21.5rem] h-[12rem]" />
+        <div className="m-3 bg-white w-[21.5rem] h-[12rem]" />
+        <div className="my-3" />
+        <div className="text-sm font-thin text-white">DoodleFilm</div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Photo/BasicFrame/Frame4w.jsx
@@ -0,0 +1,21 @@
+export default function Frame4w() {
+  return (
+    <div className="bg-black w-[28rem] h-[18.8rem]">
+      <div className="flex">
+        <div>
+          <div className="ml-3 m-2 mt-3 bg-white w-[11rem] h-[8.2rem]" />
+          <div className="ml-3 m-2 mt-3 bg-white w-[11rem] h-[8.2rem]" />
+        </div>
+        <div>
+          <div className="m-2 mt-3 bg-white w-[11rem] h-[8.2rem]" />
+          <div className="m-2 mt-3 bg-white w-[11rem] h-[8.2rem]" />
+        </div>
+        <div className="mr-1" />
+        <div className="flex flex-col mt-6 text-xs font-thin tracking-widest text-center text-white justify-">
+          <p>Doodle</p>
+          <p>Film</p>
+        </div>
+      </div>
+    </div>
+  );
+}
