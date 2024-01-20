7a5fc6da9118bb63c2269f5c439499b3d6433db7
Joo YeongJun <154403139+Ye0ngjun@users.noreply.github.com>
2024-01-20 15:17:16 +0900

    feat : 프레임 퍼블리싱

--- src/components/Photo/BasicFrame/Frame2w.jsx
+++ src/components/Photo/BasicFrame/Frame2w.jsx
@@ -1,14 +1,15 @@
-export default function Frame2w() {
+export default function Frame2w({ frameUrl }) {
   return (
-    <div>
-      <div className="bg-black w-[32rem] h-[23rem] flex">
-        <div className="m-3 bg-white w-[12rem] h-[21.5rem]" />
-        <div className="m-3 bg-white w-[12rem] h-[21.5rem]" />
-        <div className="mr-3" />
-        <div className="flex flex-col text-sm font-thin text-center text-white mt-7 justify-">
-          <p>Doodle</p>
-          <p>Film</p>
-        </div>
+    <div
+      className="bg-black w-[32rem] h-[23rem] flex bg-contain"
+      style={{ backgroundImage: `url(${frameUrl})` }}
+    >
+      <div className="m-3 bg-white w-[12rem] h-[21.5rem]" />
+      <div className="m-3 bg-white w-[12rem] h-[21.5rem]" />
+      <div className="mr-3" />
+      <div className="flex flex-col text-sm font-thin text-center text-white mt-7 justify-">
+        <p>Doodle</p>
+        <p>Film</p>
       </div>
     </div>
   );
