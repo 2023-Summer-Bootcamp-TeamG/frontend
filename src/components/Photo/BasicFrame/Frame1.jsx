76300e3efff15ff8ba4977215cbc12c48916cda7
Joo YeongJun <154403139+Ye0ngjun@users.noreply.github.com>
2024-01-26 04:59:45 +0900

    feat : 배경 선택 퍼블리싱

--- src/components/Photo/BasicFrame/Frame1.jsx
+++ src/components/Photo/BasicFrame/Frame1.jsx
@@ -1,12 +1,41 @@
-export default function Frame1({ frameUrl }) {
+/* eslint-disable jsx-a11y/img-redundant-alt */
+/* eslint-disable react/jsx-curly-brace-presence */
+/* eslint-disable import/no-unresolved */
+import ImageOne from '../Select/imageOne';
+
+export default function Frame1({
+  frameUrl,
+  onlySelect,
+  onlyFilter,
+  filterUrl,
+  applyFilter,
+}) {
+  const goFilter = {
+    filter: applyFilter,
+  };
   return (
-    <div
-      className="bg-black w-[20rem] h-[25rem] items-center flex flex-col bg-contain"
-      style={{ backgroundImage: `url(${frameUrl})` }}
-    >
-      <div className="m-3 bg-white w-[18.2rem] h-[18.2rem]" />
-      <div className="my-3" />
-      <div className="text-sm font-thin text-white">DoodleFilm</div>
+    <div>
+      <div
+        className="bg-black w-[20rem] h-[25rem] items-center flex flex-col bg-contain"
+        style={{ backgroundImage: `url(${frameUrl})` }}
+      >
+        <div className="m-3 bg-white w-[18.2rem] flex flex-col justify-center items-center h-[18.2rem] cursor-pointer">
+          {onlySelect && (
+            <ImageOne size={'w-[18.2rem] h-[18.2rem]'} filterUrl={filterUrl} />
+          )}
+          {onlyFilter && (
+            <img
+              src={filterUrl}
+              alt="image"
+              style={goFilter}
+              className="w-[18.2rem] h-[18.2rem] object-cover"
+            />
+          )}
+        </div>
+
+        <div className="my-3" />
+        <div className="text-sm font-thin text-white">DoodleFilm</div>
+      </div>
     </div>
   );
 }
--- src/components/Photo/BasicFrame/Frame2l.jsx
+++ src/components/Photo/BasicFrame/Frame2l.jsx
@@ -1,13 +1,53 @@
-export default function Frame2l({ frameUrl }) {
+/* eslint-disable jsx-a11y/img-redundant-alt */
+/* eslint-disable react/jsx-curly-brace-presence */
+/* eslint-disable import/no-unresolved */
+import ImageOne from '../Select/imageOne';
+
+export default function Frame2l({
+  frameUrl,
+  onlyFilter,
+  onlySelect,
+  filterUrl,
+  applyFilter,
+}) {
+  const goFilter = {
+    filter: applyFilter,
+  };
   return (
-    <div
-      className="bg-black w-[23rem] h-[32rem] items-center flex flex-col bg-contain "
-      style={{ backgroundImage: `url(${frameUrl})` }}
-    >
-      <div className="m-3 bg-white w-[21.5rem] h-[12rem]" />
-      <div className="m-3 bg-white w-[21.5rem] h-[12rem]" />
-      <div className="my-3" />
-      <div className="text-sm font-thin text-white">DoodleFilm</div>
+    <div>
+      <div
+        className="bg-black w-[23rem] h-[32rem] items-center flex flex-col bg-contain "
+        style={{ backgroundImage: `url(${frameUrl})` }}
+      >
+        <div className="m-3 bg-white w-[21.5rem] h-[12rem] flex flex-col justify-center items-center cursor-pointer">
+          {onlySelect && (
+            <ImageOne size={'w-[21.5rem] h-[12rem]'} filterUrl={filterUrl} />
+          )}
+          {onlyFilter && (
+            <img
+              src={filterUrl[0]}
+              alt="image"
+              style={goFilter}
+              className="w-[21.5rem] h-[12rem] object-cover"
+            />
+          )}
+        </div>
+        <div className="m-3 bg-white w-[21.5rem] h-[12rem] flex flex-col justify-center items-center cursor-pointer">
+          {onlySelect && (
+            <ImageOne size={'w-[21.5rem] h-[12rem]'} filterUrl={filterUrl} />
+          )}
+          {onlyFilter && (
+            <img
+              src={filterUrl[1]}
+              alt="image"
+              style={goFilter}
+              className="w-[21.5rem] h-[12rem] object-cover"
+            />
+          )}
+        </div>
+        <div className="my-3" />
+        <div className="text-sm font-thin text-white">DoodleFilm</div>
+      </div>
     </div>
   );
 }
