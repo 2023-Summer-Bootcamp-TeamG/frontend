2911eac06990c3af2e44cdaed1615d1adfe0ea18
Joo YeongJun <154403139+Ye0ngjun@users.noreply.github.com>
2024-01-26 05:01:57 +0900

    feat : filter 선택 퍼블리싱

--- src/components/Photo/Fliter/FilterBtn.jsx
+++ src/components/Photo/Fliter/FilterBtn.jsx
@@ -1,14 +1,13 @@
-import frame from '../../../assets/frame/2x2_w.png';
-
 export default function FilterBtn({ filterName, filterColor1, filterColor2 }) {
   return (
-    <button type="button" className="flex-col ml-52">
-      <img src={frame} alt="frame" className="w-1/3" />
-      <div
-        className={`${filterColor1} text-${filterColor2} p-1 font-extrabold text-xl mt-2 mb-5 h-10 w-36`}
-      >
-        {filterName}
-      </div>
-    </button>
+    <div className="flex flex-col items-center justify-center ">
+      <button type="button" className="flex-col">
+        <div
+          className={`${filterColor1} text-${filterColor2} p-1 font-bold text-xl mt-2 mb-5 h-10 w-36`}
+        >
+          {filterName}
+        </div>
+      </button>
+    </div>
   );
 }
