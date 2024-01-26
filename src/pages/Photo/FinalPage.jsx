4fddd922305bbaa7f8d57f03af7c2dc46f803db2
Joo YeongJun <154403139+Ye0ngjun@users.noreply.github.com>
2024-01-27 04:08:20 +0900

    feat : 마지막 페이지 다운로드 기능 구현

--- src/pages/Photo/FinalPage.jsx
+++ src/pages/Photo/FinalPage.jsx
@@ -25,16 +25,26 @@ export default function FinalPage() {
     }
   };
 
+  const downloadImage = () => {
+    const a = document.createElement('a');
+    a.href = image;
+    a.download = 'downloaded_image.png';
+
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  };
+
   return (
     <div className="flex flex-col h-screen bg-cover bg-[url('./assets/background.png')]">
       <TeamName />
 
-      <div className="flex justify-center items-center">
+      <div className="flex items-center justify-center">
         <div className="flex justify-center items-center w-[66rem] h-[42rem] pt-20 bg-cover bg-[url('./assets/sketch.png')]">
           <RotateImg image={image} />
           <div className="flex flex-col ml-14">
             <SaveBtn title="앨범 저장" clickSaveBtn={clickSaveBtn} />
-            <SaveBtn title="다운로드" />
+            <SaveBtn title="다운로드" clickSaveBtn={downloadImage} />
           </div>
         </div>
       </div>
