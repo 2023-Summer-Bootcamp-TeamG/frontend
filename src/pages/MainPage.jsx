043edbdcdafc7a24891fee28c217a05158ebeffb
Joo YeongJun <154403139+Ye0ngjun@users.noreply.github.com>
2024-01-27 17:35:39 +0900

    feat : 메인 페이지 피드백 리퍼블리싱

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -5,14 +5,14 @@ import { Link, useNavigate } from 'react-router-dom';
 export default function MainPage() {
   const navigate = useNavigate();
   return (
-    <div className="flex flex-col h-screen justify-center items-center bg-cover py-12 bg-[url('./assets/background.png')]">
-      <div className="flex w-[68rem] h-[50rem] bg-cover bg-[url('./assets/sketch.png')]">
+    <div className="flex flex-col h-screen justify-center items-center bg-cover py-8 bg-[url('./assets/background.png')]">
+      <div className="flex w-[72rem] h-[55rem] bg-cover bg-[url('./assets/sketch.png')]">
         <div>
-          <div className="w-[25rem] h-[8rem] mr-20 mt-14 ml-[6rem] mb-[2rem] bg-cover bg-[url('./assets/logo.png')]" />
-          <div className="w-[24rem] h-[18rem] ml-[5rem] mb-[2rem] bg-cover bg-[url('./assets/whiteframe.png')]">
-            <div className="fixed mt-[4.5rem] ml-[5rem]">
+          <div className="w-[25rem] h-[8rem] mr-20 mt-24 ml-[6rem] mb-[2rem] bg-cover bg-[url('./assets/logo.png')]" />
+          <div className="w-[24rem] h-[18rem] ml-[5rem] bg-cover bg-[url('./assets/whiteframe.png')]">
+            <div className="fixed mt-[4.8rem] ml-[5.5rem]">
               <div
-                className="w-[6.4rem] h-[3.2rem] bg-cover bg-[url('./assets/login.png')] cursor-pointer"
+                className="w-[5.5rem] h-[2.8rem] bg-cover bg-[url('./assets/login.png')] cursor-pointer"
                 onClick={() => {
                   navigate('/login');
                 }}
@@ -20,23 +20,28 @@ export default function MainPage() {
             </div>
             <div className="fixed mt-[11rem] ml-[13.5rem]">
               <div
-                className="w-[6rem] h-[3rem] bg-cover bg-[url('./assets/album.png')] cursor-pointer"
+                className="w-[5.5rem] h-[2.8rem] bg-cover bg-[url('./assets/album.png')] cursor-pointer"
                 onClick={() => {
                   navigate('/album');
                 }}
               />
             </div>
-            <div className="fixed mt-[18rem] ml-[11rem]">
-              <div className="w-[18rem] h-[15rem] bg-cover bg-[url('./assets/pencil.png')]" />
-            </div>
+          </div>
+
+          <div className="fixed ml-[17.6rem]">
+            <div className="w-[18rem] h-[14rem] bg-cover bg-[url('./assets/pencil.png')] " />
           </div>
           <Link to="/photo/basicFrame">
-            <div className="flex justify-center items-center">
+            {/* <div className="fixed ml-[14rem] mt-[1.3rem] group">
+              <div className="w-[10rem] h-[6rem] bg-cover bg-[url('./assets/start.png')] transform group-hover:scale-105" />
+            </div> */}
+            <div className="fixed ml-[14rem] mt-[1.3rem]">
               <div className="w-[10rem] h-[6rem] bg-cover bg-[url('./assets/start.png')]" />
             </div>
           </Link>
         </div>
-        <div className="w-[25rem] h-[30rem] bg-cover mt-24 bg-[url('./assets/rabbit.png')]" />
+
+        <div className="w-[28rem] h-[35rem] bg-cover mt-24 bg-[url('./assets/rabbit.png')]" />
       </div>
     </div>
   );
