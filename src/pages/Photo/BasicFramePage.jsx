d66ba7eff4d3c10779f6f155dbbce6217a6cf912
Joo YeongJun <154403139+Ye0ngjun@users.noreply.github.com>
2024-01-28 04:30:44 +0900

    feat : framer-motion 메인페이지/배경페이지만 적용

--- src/pages/Photo/BasicFramePage.jsx
+++ src/pages/Photo/BasicFramePage.jsx
@@ -1,5 +1,8 @@
+/* eslint-disable react/no-unstable-nested-components */
+/* eslint-disable react/function-component-definition */
 /* eslint-disable simple-import-sort/imports */
 import { useEffect, useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 import Cut1 from '../../components/Photo/BasicFrame/Cut1';
 import Cut2 from '../../components/Photo/BasicFrame/Cut2';
@@ -11,6 +14,17 @@ import TeamName from '../../components/Common/TeamName';
 export default function BasicFramePage() {
   const [select, setSelect] = useState('');
   const { setImageUrl } = useImageStore();
+
+  const PageTransition = ({ children }) => (
+    <motion.div
+      initial={{ y: '-100%' }}
+      animate={{ y: 0 }}
+      exit={{ y: '100%' }}
+    >
+      {children}
+    </motion.div>
+  );
+
   useEffect(() => {
     setImageUrl('');
   }, []);
@@ -19,26 +33,28 @@ export default function BasicFramePage() {
       <div>
         <TeamName />
       </div>
-      <div className="flex justify-center items-center">
-        <div className="flex flex-col w-[66rem] h-[42rem] bg-cover bg-[url('./assets/sketch.png')]">
-          <div className="m-4" />
-          <div className="flex justify-center">
-            <div className="text-xl m-14">
-              <CutBtn select={select} setSelect={setSelect} content="1Cut" />
+      <div className="flex items-center justify-center">
+        <PageTransition>
+          <div className="flex flex-col w-[66rem] h-[42rem] bg-cover bg-[url('./assets/sketch.png')]">
+            <div className="m-4" />
+            <div className="flex justify-center">
+              <div className="text-xl m-14">
+                <CutBtn select={select} setSelect={setSelect} content="1Cut" />
+              </div>
+              <div className="text-xl m-14">
+                <CutBtn select={select} setSelect={setSelect} content="2Cut" />
+              </div>
+              <div className="text-xl m-14">
+                <CutBtn select={select} setSelect={setSelect} content="4Cut" />
+              </div>
             </div>
-            <div className="text-xl m-14">
-              <CutBtn select={select} setSelect={setSelect} content="2Cut" />
-            </div>
-            <div className="text-xl m-14">
-              <CutBtn select={select} setSelect={setSelect} content="4Cut" />
+            <div className="flex justify-around">
+              {select === '1Cut' && <Cut1 />}
+              {select === '2Cut' && <Cut2 />}
+              {select === '4Cut' && <Cut4 />}
             </div>
           </div>
-          <div className="flex justify-around">
-            {select === '1Cut' && <Cut1 />}
-            {select === '2Cut' && <Cut2 />}
-            {select === '4Cut' && <Cut4 />}
-          </div>
-        </div>
+        </PageTransition>
       </div>
 
       <div className="m-1" />
