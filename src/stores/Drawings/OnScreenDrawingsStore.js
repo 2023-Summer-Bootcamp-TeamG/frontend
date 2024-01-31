07c570bd68c58d255af6681a3023e02b6602981e
hwang <hgh1057@gmail.com>
2024-01-31 19:51:39 +0900

    feat: canavas변경

---
+++ src/stores/Drawings/OnScreenDrawingsStore.js
@@ -0,0 +1,22 @@
+import { create } from 'zustand';
+
+const useDrawingStore = create((set) => ({
+  drawings: [],
+
+  addDrawing: (drawing) =>
+    set((state) => ({ drawings: [...state.drawings, drawing] })),
+
+  updateDrawing: (index, newProps) =>
+    set((state) => ({
+      drawings: state.drawings.map((drawing, i) =>
+        i === index ? { ...drawing, ...newProps } : drawing,
+      ),
+    })),
+
+  removeDrawing: (index) =>
+    set((state) => ({
+      drawings: state.drawings.filter((_, i) => i !== index),
+    })),
+}));
+
+export default useDrawingStore;
