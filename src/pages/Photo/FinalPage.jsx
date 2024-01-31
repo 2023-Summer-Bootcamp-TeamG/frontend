ee01914b06af4e24a753e64567090f051bd0458a
hwang <hgh1057@gmail.com>
2024-02-01 00:49:15 +0900

    feat: qr연동

--- src/pages/Photo/FinalPage.jsx
+++ src/pages/Photo/FinalPage.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable object-shorthand */
+import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 import apiV1Instance from '../../api/api-instance';
@@ -8,23 +9,25 @@ import SaveBtn from '../../components/Photo/Final/SaveBtn';
 import useDrawingStore from '../../stores/Drawings/OnScreenDrawingsStore';
 import useStickerStore from '../../stores/Stickers/OnScreenStickersStore';
 import useTextStore from '../../stores/Text/OnScreenTextStore';
-import CanvasWithDrawings from '../T3';
 
 export default function FinalPage() {
   const location = useLocation();
-  const { title, image } = location.state || {};
+  const { title, image, capturedData, photoId } = location.state || {};
   const { drawings } = useDrawingStore();
   const { stickers } = useStickerStore();
   const { texts } = useTextStore();
+  const [qr, setQr] = useState();
 
   console.log('draw : ', drawings);
   console.log('stickers : ', stickers);
   console.log('texts : ', texts);
+  console.log('cap', capturedData);
+  console.log('qrId:', photoId);
 
   const clickSaveBtn = async () => {
     const data = {
       title: title,
-      photo_data: image,
+      photo_data: capturedData,
       result_photo_data: image,
       stickers: stickers,
       textboxes: texts,
@@ -48,15 +51,34 @@ export default function FinalPage() {
     document.body.removeChild(a);
   };
 
+  const getQr = async () => {
+    try {
+      const response = await apiV1Instance.get(`/photos/qr/${photoId}/`);
+      setQr(response.data.qr_code);
+      console.log(response.data.qr_code);
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  useEffect(() => {
+    getQr();
+  }, []);
+
   return (
     <div className="flex flex-col h-screen bg-cover bg-[url('./assets/background.png')]">
       <TeamName />
 
       <div className="flex items-center justify-center">
-        <CanvasWithDrawings drawings={drawings} width={800} height={600} />
         <div className="flex justify-center items-center w-[66rem] h-[42rem] pt-20 bg-cover bg-[url('./assets/sketch.png')]">
           <RotateImg image={image} />
-          <div className="flex flex-col ml-14">
+          <div className="flex flex-col ml-14 items-center justify-center">
+            <img
+              src={`data:image/png;base64,${qr}`}
+              alt="QR Code"
+              className="h-[12rem] w-[12rem] mb-5"
+            />
+
             <SaveBtn title="앨범 저장" clickSaveBtn={clickSaveBtn} />
             <SaveBtn title="다운로드" clickSaveBtn={downloadImage} />
           </div>
